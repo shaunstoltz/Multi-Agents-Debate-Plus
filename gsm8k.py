08dfea0c22485f653e57c9b8916a0bc36d9312dc
Shaun Stoltz <shaunstoltz@hotmail.com>
2023-06-03 17:39:04 -0400

    Bug fix

--- gsm8k.py
+++ gsm8k.py
@@ -202,6 +202,7 @@ class Debate:
             judge_player.add_event(self.config['judge_prompt_last2'])
             ans = judge_player.ask()
             judge_player.add_memory(ans)
+            print("==========and =========================>>>>> ",ans)
             ans = json.loads(ans)
             print("============================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ans ==>> ",ans)
             if ans["debate_answer"] != '':
